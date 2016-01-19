8448302468e84b34d4b949808dd5874149be15d1
TJ Holowaychuk <tj@vision-media.ca>
2016-01-19 10:14:31 -0800

    add Semaphore.Run(fn) helper
    same pattern over and over anyway

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -1,17 +1,31 @@
+// Package semaphore implements a channel-based semaphore.
 package semaphore
 
+// Semaphore channel.
 type Semaphore chan struct{}
 
+// Acquire resource.
 func (s Semaphore) Acquire() {
 	s <- struct{}{}
 }
 
+// Release resource.
 func (s Semaphore) Release() {
 	<-s
 }
 
+// Wait for completion.
 func (s Semaphore) Wait() {
 	for i := 0; i < cap(s); i++ {
 		s <- struct{}{}
 	}
+}
+
+// Run `fn` in a goroutine, acquiring then releasing after its return.
+func (s Semaphore) Run(fn func()) {
+	s.Acquire()
+	go func() {
+		defer s.Release()
+		fn()
+	}()
 }
